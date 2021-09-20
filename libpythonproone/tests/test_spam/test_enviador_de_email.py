05b8662e61f8b1c326a6bd9b636dc7d8d724ddb2
Emanuel <emanuelfilipess@gmail.com>
2021-09-19 21:22:49 -0300

    Acrescentado teste de execução

---
+++ libpythonproone/tests/test_spam/test_enviador_de_email.py
@@ -0,0 +1,38 @@
+import pytest
+
+from libpythonproone.spam.enviador_de_email import Enviador, EmailInvalido
+
+
+def test_criar_enviador_de_email():
+    enviador = Enviador()
+    assert enviador is not None
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['emanuelfilipess@gmail.com', 'foo@bar.com.br']
+)
+def test_remetente(remetente):
+    enviador = Enviador()
+    resultado = enviador.enviar(
+        remetente,
+        'manel_scout@hotmail.com',
+        'Cursos Python Pro',
+        'Primeira turma Guido Von Rossum aberta.'
+    )
+    assert remetente in resultado
+
+
+@pytest.mark.parametrize(
+    'remetente',
+    ['', 'emanuel']
+)
+def test_remetente_invalido(remetente):
+    enviador = Enviador()
+    with pytest.raises(EmailInvalido):
+        enviador.enviar(
+            remetente,
+            'manel_scout@hotmail.com',
+            'Cursos Python Pro',
+            'Primeira turma Guido Von Rossum aberta.'
+        )
